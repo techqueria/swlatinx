90e2b699aca18c8ae76681de6b14d0a9d53a0552
David Silva <david.silva@dokbid.com>
2017-08-31 20:07:43 -0400

    fixed a bit the contact page

--- _public/scripts/freelancer.js
+++ _public/scripts/freelancer.js
@@ -5,11 +5,11 @@
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
+        //var $anchor = $(this);
+        // $('html, body').stop().animate({
+        //    scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        //}, 1250, 'easeInOutExpo');
+        //event.preventDefault();
     });
 
     // Highlight the top nav as scrolling occurs
